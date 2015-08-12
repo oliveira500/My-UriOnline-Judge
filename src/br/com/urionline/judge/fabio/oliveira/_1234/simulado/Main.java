a490232e5e09a6651033e6443befc4eebe9735ab
Fabio Oliveira <ff.oliveira@samsung.com>
2015-08-12 14:18:24 -0400

    Resolved the problem: 1234: Dancing Sentence

---
+++ src/br/com/urionline/judge/fabio/oliveira/_1234/simulado/Main.java
@@ -0,0 +1,37 @@
+package br.com.urionline.judge.fabio.oliveira._1234.simulado;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fabio Oliveira
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+	try (Scanner myInput = new Scanner(System.in)) {
+	    while (myInput.hasNext()) {
+		String string = myInput.nextLine();
+
+		StringBuilder stringBuilder = new StringBuilder(string);
+		boolean myFlag = true;
+		for (int index = 0; index < stringBuilder.length(); index++) {
+		    if (stringBuilder.charAt(index) == ' ') {
+			continue;
+		    } else if (myFlag) {
+			stringBuilder.setCharAt(index, Character
+				.toUpperCase(stringBuilder.charAt(index)));
+		    } else {
+			stringBuilder.setCharAt(index, Character
+				.toLowerCase(stringBuilder.charAt(index)));
+		    }
+		    myFlag = !myFlag;
+		}
+		System.out.println(stringBuilder.toString());
+	    }
+	}
+
+    }
+
+}
